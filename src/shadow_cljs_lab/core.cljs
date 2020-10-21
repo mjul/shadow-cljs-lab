c83ab0c35fd3d886d9c263b26284440665e7fdf7
Martin Jul <martin@mjul.com>
2020-10-21 12:35:15 +0000

    Add example Ant D Empty component

--- src/shadow_cljs_lab/core.cljs
+++ src/shadow_cljs_lab/core.cljs
@@ -1,14 +1,20 @@
 (ns shadow-cljs-lab.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            ["antd/es/empty" :default ant-empty]))
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(def aempty (reagent.core/adapt-react-class ant-empty))
+
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit and watch it change!"]])
+   [:h2 "Edit and watch it change!"]
+   [:h3 "Manually wrapped Ant Design Component"]
+   [aempty]
+   [:div "Done"]])
 
 (defn start []
   (reagent/render-component [hello-world]
