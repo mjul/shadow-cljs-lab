93847ea3e82c5c8ceff1ed22eb19269dc639684f
Martin Jul <martin@mjul.com>
2020-10-21 21:31:45 +0000

    Add some icons

--- src/shadow_cljs_lab/core.cljs
+++ src/shadow_cljs_lab/core.cljs
@@ -1,10 +1,11 @@
 (ns shadow-cljs-lab.core
-;; There is also an Ant Design component called empty:
-  (:refer-clojure :exclude [empty])  
   (:require [reagent.core :as reagent :refer [atom]]
             [syn-antd.layout :refer [layout layout-sider layout-header layout-content layout-footer]]
             [syn-antd.menu :refer [menu menu-item menu-sub-menu]]
-            [syn-antd.empty :refer [empty]]))
+            [syn-antd.empty]
+            [syn-antd.icons.folder-outlined :refer [folder-outlined]]
+            [syn-antd.icons.file-word-outlined :refer [file-word-outlined]]
+            [syn-antd.icons.file-pdf-outlined :refer [file-pdf-outlined]]))
 
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
@@ -16,9 +17,9 @@
     [:div {:class "logo"} "Lab!"]
     [menu {:mode :inline :theme :dark}
      [menu-item {:key 1} "Home"]
-     [menu-sub-menu {:key 2 :title "Documents"}
-      [menu-item {:key 21} "Foo.docx"]
-      [menu-item {:key 22} "Bar.docx"]]]]
+     [menu-sub-menu {:key 2 :title (reagent/as-component [:span [folder-outlined] "Documents"])}
+      [menu-item {:key 21} [file-word-outlined] "Foo.docx"]
+      [menu-item {:key 22} [file-pdf-outlined] "Bar.pdf"]]]]
    [layout
     [layout-header {:style {:background :yellow}}
      "Submarine"]
@@ -26,7 +27,7 @@
      [:div
       [:h1 (:text @app-state)]
       [:h2 "Edit and watch it change!"]
-      [empty]
+      [syn-antd.empty/empty]
       [:div "Done"]]]
     [layout-footer {:style {:background :white :color :grey}}
      "This is the footer"]
